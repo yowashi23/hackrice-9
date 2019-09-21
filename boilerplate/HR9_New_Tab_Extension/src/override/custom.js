4473e2c2a1c5ed33838f1d715f4e7006a35d1d3b
yowashi23 <shin.yongwhan23@gmail.com>
2019-09-21 15:43:44 -0500

    cards nearly completed

--- boilerplate/HR9_New_Tab_Extension/src/override/custom.js
+++ boilerplate/HR9_New_Tab_Extension/src/override/custom.js
@@ -1,1 +1,9 @@
-custom.js
+var d = new Date(),
+    h = d.getHours(),
+    i;
+
+if (h < 6) i = "night.jpg";
+else if (h < 9) i = "day.jpg";
+else if (h < 15) i = "day.jpg";
+else if (h < 21) i = "day.jpg";
+else i = "night.jpg";
