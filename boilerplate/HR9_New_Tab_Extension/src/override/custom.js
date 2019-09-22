62ff7b9f0cf5fa2d04d985563148efae3efd38a6
yowashi23 <shin.yongwhan23@gmail.com>
2019-09-22 03:57:02 -0500

    finished finally

--- boilerplate/HR9_New_Tab_Extension/src/override/custom.js
+++ boilerplate/HR9_New_Tab_Extension/src/override/custom.js
@@ -5,7 +5,40 @@ document.addEventListener('DOMContentLoaded', function () {
 function main() {
   // Get the DOM reference
   var contentId = document.getElementById("content");
+  var contentId1 = document.getElementById("content1");
+  var contentId2 = document.getElementById("content2");
+  var contentId3 = document.getElementById("content3");
+  var contentId4 = document.getElementById("content4");
+  var contentId5 = document.getElementById("content5");
+  var contentId6 = document.getElementById("content6");
+  var contentId7 = document.getElementById("content7");
+  var contentId8 = document.getElementById("content8");
+  var contentId9 = document.getElementById("content9");
+  var contentId10 = document.getElementById("content10");
+  var contentId11 = document.getElementById("content11");
   // Toggle 
   contentId.style.display == "none" ? contentId.style.display = "block" : 
   contentId.style.display = "none"; 
+  contentId1.style.display == "none" ? contentId1.style.display = "block" : 
+  contentId1.style.display = "none"; 
+  contentId2.style.display == "none" ? contentId2.style.display = "block" : 
+  contentId2.style.display = "none"; 
+  contentId3.style.display == "none" ? contentId3.style.display = "block" : 
+  contentId3.style.display = "none"; 
+  contentId4.style.display == "none" ? contentId4.style.display = "block" : 
+  contentId4.style.display = "none"; 
+  contentId5.style.display == "none" ? contentId5.style.display = "block" : 
+  contentId5.style.display = "none"; 
+  contentId6.style.display == "none" ? contentId6.style.display = "block" : 
+  contentId6.style.display = "none"; 
+  contentId7.style.display == "none" ? contentId7.style.display = "block" : 
+  contentId7.style.display = "none"; 
+  contentId8.style.display == "none" ? contentId8.style.display = "block" : 
+  contentId8.style.display = "none"; 
+  contentId9.style.display == "none" ? contentId9.style.display = "block" : 
+  contentId9.style.display = "none"; 
+  contentId10.style.display == "none" ? contentId10.style.display = "block" : 
+  contentId10.style.display = "none"; 
+  contentId11.style.display == "none" ? contentId11.style.display = "block" : 
+  contentId11.style.display = "none"; 
 }
