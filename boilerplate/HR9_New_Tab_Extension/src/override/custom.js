681ee02684e34efeab6578411023f983b7dcffac
yowashi23 <shin.yongwhan23@gmail.com>
2019-09-21 19:20:47 -0500

    transparent cards

--- boilerplate/HR9_New_Tab_Extension/src/override/custom.js
+++
@@ -1,9 +0,0 @@
-var d = new Date(),
-    h = d.getHours(),
-    i;
-
-if (h < 6) i = "night.jpg";
-else if (h < 9) i = "day.jpg";
-else if (h < 15) i = "day.jpg";
-else if (h < 21) i = "day.jpg";
-else i = "night.jpg";
